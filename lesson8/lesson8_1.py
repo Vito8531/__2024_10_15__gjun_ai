2bb73cfe10d0b26a0ed89ab3df8f413e544a22ce
Vito8531 <a0213knight@gmail.com>
2024-11-14 13:25:35 +0000

    lesson8_04

--- lesson8/lesson8_1.py
+++ lesson8/lesson8_1.py
@@ -6,9 +6,23 @@ def get_names(nums:int=2)->list[str]:
     names = random.choices(names,k=nums)
     return names
 
+#nums = int(input('請輸入學生數量(最多10位):'))
+#student_names = get_names(nums=nums)
+#for name in student_names:
+#    print(name)
 
+
+def generate_student(names):
+    students=[]
+    for name in names:
+        chinese = random.randint(0,100)
+        english = random.randint(0,100)
+        math = random.randint(0,100)
+        stu = {"name":name,"chinese":chinese,"english":english,"math":math}
+        students.append(stu)
+    return students
 
 nums = int(input('請輸入學生數量(最多10位):'))
 student_names = get_names(nums=nums)
-for name in student_names:
-    print(name)
+students = generate_student(names=student_names)
+print(students)
