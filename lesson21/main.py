cc6c22f4f9e7a222e231e6a2046ddfe0872dc9bf
Vito8531 <a0213knight@gmail.com>
2025-01-01 03:25:43 +0000

    lessono21_main_1

--- lesson21/main.py
+++ lesson21/main.py
@@ -3,12 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/<string:name>")
-def hello_world(name:str=""):
-    #return "<p>Hello, World!</p>"
-    #return "<h1>Hello, 您好歡迎進入!</h1>"
-    return f"<h1>Hello,{name}您好!</h1>"
+def hello_world():
+    return "<p>Hello, World!</p>"
+    
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
+#@app.route('/hello')
+#def hello():
+#    return 'Hello, World'
